e702705e079ec870a0fefecec22142d743e8f0ee
ZainabbHaider <zh07104@st.habib.edu.pk>
2024-06-26 21:05:33 +0500

    Initial code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Login from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Register from "./Pages/Register";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar isLoggedIn={isLoggedIn} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
