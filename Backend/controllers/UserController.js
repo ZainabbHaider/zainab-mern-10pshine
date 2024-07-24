403ceb14275290c4c5b5091e348804924fc291ad
ZainabbHaider <zh07104@st.habib.edu.pk>
2024-07-24 16:57:43 +0500

    Adding profile functions

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -60,4 +60,46 @@ const login = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, login };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (email) user.email = email;
+    if (password) user.password = password;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      firstName: updatedUser.firstName,
+      lastName: updatedUser.lastName,
+      email: updatedUser.email,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+module.exports = { registerUser, login, getUserProfile, updateUserProfile };
