6f002692be136e7736a59b936a6a5af4cc4a185d
ZainabbHaider <zh07104@st.habib.edu.pk>
2024-07-24 16:44:51 +0500

    Adding note edit route

--- Backend/routes/notesRoute.js
+++ Backend/routes/notesRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middleware/jwt')
-const { deleteNote, addNote, showNotes } = require('../controllers/NotesController');
+const { deleteNote, addNote, showNotes, editNote } = require('../controllers/NotesController');
 
 
 router.route("/").get(protect, showNotes)
 router.route("/add").post(protect, addNote)
-router.route('/:id').delete(protect, deleteNote);
+router.route('/:id').delete(protect, deleteNote).put(protect, editNote);
 
 
 module.exports = router; 
