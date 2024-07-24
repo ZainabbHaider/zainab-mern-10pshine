d8268ae45163862605baedb872b8fe765542ea29
ZainabbHaider <zh07104@st.habib.edu.pk>
2024-07-24 17:00:25 +0500

    Adding profile funtion routes

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { registerUser, login } = require('../controllers/UserController');
+const { registerUser, login, getUserProfile, updateUserProfile } = require('../controllers/UserController');
+const { protect } = require('../middleware/jwt');
 const router = express.Router();
 
 router.route("/").post(registerUser)
 router.route("/login").post(login)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile); 
+
 
 module.exports = router; 
